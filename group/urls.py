b5e476c368e3c88179d71b85e56d8e2cd2d62a8c
= <ulim130@naver.com>
2019-08-05 22:05:30 +0900

    group

--- group/urls.py
+++ group/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import GroupViewSet, get_check, ParticipateViewSet, get_check_nick, del_member
+from .views import GroupViewSet, get_check, ParticipateViewSet, ParticipatedGroupViewSet, create_group, get_check_nick, del_member
 
+# 방 검색
 router = DefaultRouter()
 router.register('', GroupViewSet)
-part_list = ParticipateViewSet.as_view({
-    'get': 'list',
-    'post': 'create',
-})
+
+# 방 참여
+part = ParticipateViewSet.as_view({'post': 'create'})
+
+# 참여중인 방 조회
+part_list = ParticipatedGroupViewSet.as_view({'get': 'list'})
+
+
 part_detail = ParticipateViewSet.as_view({
     'get': 'retrieve',
     'put': 'update',
@@ -15,10 +20,13 @@ part_detail = ParticipateViewSet.as_view({
     'delete': 'destroy',
 })
 
+
 urlpatterns = [
     path('', include(router.urls)),
     path('namecheck/<str:pk>', get_check),
-    path('sign', part_list),
+    path('create', create_group),
+    path('signed', part_list),
+    path('sign', part),
     path('sign/<int:pk>', part_detail),
     path('check/<int:GroupPid>/<str:Nickname>/<str:member_id>/<str:GroupPassword>', get_check_nick),
     path('delete/<int:GroupPid>/<str:member_id>', del_member),
